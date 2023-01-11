cb10b8edf08078ace06b5f64911f35e8e9df0cbc
Faks03 <ifeoluwafakolade@gmail.com>
2023-01-11 08:38:08 +0100

    The Login Page features added

---
+++ LoginPage.xaml.cs
@@ -0,0 +1,26 @@
+namespace Time_table;
+
+public partial class LoginPage : ContentPage
+{ 
+    public LoginPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnLoginClicked(object sender, EventArgs e)
+    {
+        string email = txtEmail.Text;
+        string password = txtPassword.Text;
+
+        if (email == "admin" && password == "admin")
+        {
+            Navigation.PushAsync(new MainPage());
+        }
+        else
+        {
+            DisplayAlert("Error", "Invalid username or password", "OK");
+        }
+
+        // SemanticScreenReader.Announce(CounterBtn.Text);
+    }
+}
