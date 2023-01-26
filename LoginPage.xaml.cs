d9e7363463a145640baa67e3a48fbc86aefb610e
Toluwani omosebi <enochjesse884@outlook.com>
2023-01-26 02:14:57 +0100

    all routes linked from login page

--- LoginPage.xaml.cs
+++ LoginPage.xaml.cs
@@ -6,15 +6,32 @@ public partial class LoginPage : ContentPage
     {
         InitializeComponent();
     }
+    //create a function to check the database for the user and password, if both are correct then check their roles and navigate to the correct page
 
     private void OnLoginClicked(object sender, EventArgs e)
     {
         string email = txtEmail.Text;
         string password = txtPassword.Text;
 
+        // sample code will change the inside if statement to become if (role == "admin") then navigate to the admin page, if (role == "lecturer") then navigate to the lecturer page, if (role == "student") then navigate to the student page.
+
         if (email == "admin" && password == "admin")
         {
-            Navigation.PushAsync(new MainPage());
+            // Display login success message thenn navigate to the admin page
+            DisplayAlert("Success", "Login successful", "OK");
+            Navigation.PushAsync(new Admin_Page.AdminHomePage());
+        }
+        else if (email == "lecturer" && password == "lecturer")
+        {
+            // Display login success message thenn navigate to the lecturer page
+            DisplayAlert("Success", "Login successful", "OK");
+            Navigation.PushAsync(new Lecturer_Page.HomePage());
+        }
+        else if (email == "student" && password == "student")
+        {
+            // Display login success message thenn navigate to the student page
+            DisplayAlert("Success", "Login successful", "OK");
+            Navigation.PushAsync(new Student_Page.StuHomePage());
         }
         else
         {
