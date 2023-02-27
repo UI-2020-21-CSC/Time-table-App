3902b462cb89c03b059c80cae4b9da8e3b6744e0
PeaceTem <olumidejonathan10@gmail.com>
2023-02-27 16:51:52 +0100

    Added the base attributes for the view model

---
+++ ViewModel/BaseViewModel.cs
@@ -0,0 +1,50 @@
+﻿
+//using CommunityToolkit.Mvvm.ComponentModel;
+//using CommunityToolkit.Mvvm.Input;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Time_table.ViewModel
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        bool isBusy;
+        string title;
+        // Get the Community ToolKit From the Nuget Packet Manager Later
+        public bool IsBusy
+        {
+            get => isBusy;
+
+            set
+            {
+                if (isBusy == value)
+                    return;
+
+                isBusy = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(IsNotBusy));
+            }
+        }
+
+        public string Title
+        {
+            get => title;
+
+            set
+            {
+                if (title == value) return;
+
+                title = value;
+                OnPropertyChanged();
+            }
+        }
+        public bool IsNotBusy => !IsBusy;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
