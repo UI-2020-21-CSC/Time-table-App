821bb6850555746e6519480a08eb7d54f7c7edf6
PeaceTem <olumidejonathan10@gmail.com>
2023-02-27 16:21:27 +0100

    added the EnlistedCoursessErivce to fetch the data from the file

---
+++ Services/Student/EnlistedCoursesService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Time_table.Services.Student
+{
+    class EnlistedCoursesService
+    {
+    }
+}
