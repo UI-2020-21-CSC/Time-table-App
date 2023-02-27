cf2c86ad1fab3dd75ca4cbf703b70c748940d712
PeaceTem <olumidejonathan10@gmail.com>
2023-02-27 16:36:07 +0100

    added the view model as the binding context for the enlisted courses page

--- Student_Page/EnlistedCourses.xaml.cs
+++ Student_Page/EnlistedCourses.xaml.cs
@@ -1,9 +1,12 @@
+using Time_table.ViewModel.Student;
+
 namespace Time_table.Student_Page;
 
 public partial class EnlistedCourses : ContentPage
 {
-	public EnlistedCourses()
-	{
-		InitializeComponent();
-	}
+    public EnlistedCourses(EnlistedCoursesViewModel viewModel)
+    {
+        InitializeComponent();
+        BindingContext = viewModel;
+    }
 }
