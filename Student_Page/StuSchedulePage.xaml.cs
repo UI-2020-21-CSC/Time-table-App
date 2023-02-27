4cc2953d8bdf3711296c1e423fb5f45991beb4cd
PeaceTem <olumidejonathan10@gmail.com>
2023-02-27 16:44:44 +0100

    Added the calendar for the schedule page

--- Student_Page/StuSchedulePage.xaml.cs
+++ Student_Page/StuSchedulePage.xaml.cs
@@ -1,9 +1,31 @@
-namespace Time_table.Student_Page;
-
-public partial class StuSchedulePage : ContentPage
+//using Syncfusion.Maui.ListView;
+using Syncfusion.Maui.Calendar;
+namespace Time_table.Student_Page
 {
-	public StuSchedulePage()
-	{
-		InitializeComponent();
-	}
+
+    public partial class StuSchedulePage : ContentPage
+    {
+        public StuSchedulePage()
+        {
+            InitializeComponent();
+
+        }
+
+
+
+        //this.Calendar.ViewChanged += OnCalendarViewChanged;
+        private void OnDaySelectionChanged(object sender, CalendarSelectionChangedEventArgs e)
+        {
+            var oldDateTime = e.OldValue;
+            var newDateTime = e.NewValue;
+        }
+
+        // disable the weekend dates
+        //this.Calendar.Tapped += OnCalendarTapped;
+        private void OnDayTapped(object sender, CalendarTappedEventArgs e)
+        {
+            var selectedDate = e.Date;
+            var calendarElement = e.Element;
+        }
+    }
 }
