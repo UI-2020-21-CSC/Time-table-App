f34ae7b2df7bcafec8bfe2cdc69787faa0f77da9
PeaceTem <olumidejonathan10@gmail.com>
2023-02-27 16:58:03 +0100

    Added the view model for the student schedule page

---
+++ ViewModel/Student/StuScheduleViewModel.cs
@@ -0,0 +1,56 @@
+﻿
+using System.Collections.ObjectModel;
+using Time_table.Services.Student;
+using Time_table.Model;
+
+
+namespace Time_table.ViewModel.Student
+{
+    class StuScheduleViewModel : BaseViewModel
+    {
+
+        public ObservableCollection<Course> Courses { get; } = new ObservableCollection<Course>();
+
+        public Command GetDailyCoursesCommand { get; }
+
+        StuScheduleService dailyCoursesService;
+
+
+        public StuScheduleViewModel(StuScheduleService coursesService)
+        {
+            dailyCoursesService = coursesService;
+            Title = "Schedule";
+            GetDailyCoursesCommand = new Command(async () => await GetDailyCoursesAsync());
+        }
+
+        async Task GetDailyCoursesAsync()
+        {
+            if (IsBusy)
+                return;
+
+
+            try
+            {
+                IsBusy = true;
+                var courses = await dailyCoursesService.GetDailyCourses();
+
+                if (Courses.Count != 0)
+                    Courses.Clear();
+
+                foreach (var course in courses)
+                    Courses.Add(course);
+
+            }
+            catch (Exception ex)
+            {
+                //Debug.WriteLine($"Unable to get monkeys: {ex.Message}");
+                await Shell.Current.DisplayAlert("Error!", ex.Message, "OK");
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+
+        }
+    }
+}
