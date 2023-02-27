7c01c50f15084778e91c4a2263b585dd2033be2c
PeaceTem <olumidejonathan10@gmail.com>
2023-02-27 16:41:52 +0100

    Added the navigation to all the pages in the student pages

--- Student_Page/StuHomePage.xaml.cs
+++ Student_Page/StuHomePage.xaml.cs
@@ -1,9 +1,37 @@
+using Time_table.Services.Student;
+using Time_table.ViewModel.Student;
+
 namespace Time_table.Student_Page;
 
 public partial class StuHomePage : ContentPage
 {
-	public StuHomePage()
-	{
-		InitializeComponent();
-	}
+    public StuHomePage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnHomeClicked(object sender, EventArgs e)
+    {
+        // Go to the student home page
+        Navigation.PushAsync(new Student_Page.StuHomePage());
+
+    }
+    private void OnScheduleClicked(object sender, EventArgs e)
+    {
+        // Go to the student schedule page
+        Navigation.PushAsync(new Student_Page.StuSchedulePage());
+
+    }
+
+
+
+    private void OnEnlistedCoursesClicked(object sender, EventArgs e)
+    {
+        // Go to the student enlisted courses page
+        StuScheduleService service = new StuScheduleService();
+        EnlistedCoursesViewModel viewModel = new EnlistedCoursesViewModel(service);
+        Navigation.PushAsync(new Student_Page.EnlistedCourses(viewModel));
+
+    }
 }
+
