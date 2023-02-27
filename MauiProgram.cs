8ba354545c0817fa923ae562455bb578c34cb434
PeaceTem <olumidejonathan10@gmail.com>
2023-02-27 17:15:46 +0100

    Added dependency injections

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,18 +1,38 @@
-﻿namespace Time_table;
+﻿using Syncfusion.Maui.Core.Hosting;
+using Time_table.Services.Student;
+using Time_table.ViewModel.Student;
+using Time_table.ViewModel;
+using CommunityToolkit.Maui;
 
-public static class MauiProgram
+
+namespace Time_table
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            //Adding Syncfusion Controls
+            builder.ConfigureSyncfusionCore();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                }).UseMauiCommunityToolkit();
+            //builder.ConfigureSyncfusionListView();
 
-		return builder.Build();
-	}
+            //registering with dependency injection services
+            builder.Services.AddSingleton<StuScheduleService>();
+
+
+
+            builder.Services.AddSingleton<EnlistedCoursesService>();
+
+            builder.Services.AddSingleton<EnlistedCoursesViewModel>();
+            builder.Services.AddSingleton<BaseViewModel>();
+            return builder.Build();
+        }
+    }
 }
