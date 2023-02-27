8227f0e4a8920b7a206f13bc16d4f2ffeaab2d5b
PeaceTem <olumidejonathan10@gmail.com>
2023-02-27 16:23:43 +0100

    Added the function to get the data from the file

--- Services/Student/EnlistedCoursesService.cs
+++ Services/Student/EnlistedCoursesService.cs
@@ -1,12 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
-namespace Time_table.Services.Student
+
+using Time_table.Model;
+using System.Text.Json;
+
+
+
+namespace Time_table.Services.Student;
+
+public class EnlistedCoursesService
 {
-    class EnlistedCoursesService
+    HttpClient httpClient;
+
+    List<Course> courseList = new List<Course>();
+
+    public EnlistedCoursesService()
     {
+        httpClient = new HttpClient();
+    }
+
+    public async Task<List<Course>> GetCourses()
+    {
+        //Online get request will be made to the backend to fetch the data asynchronously
+        /*
+        var url = "https://neugott.com/courses.json";
+
+        var response = await httpClient.GetAsync(url);
+
+        if ( response.IsSuccessStatusCode)
+        {
+            courseList = await response.Content.ReadFromJsonAsync<List<Course>>();
+        }
+        */
+
+        // Offline
+        using var stream = await FileSystem.OpenAppPackageFileAsync("EnlistedCourses.json");
+        using var reader = new StreamReader(stream);
+        var contents = await reader.ReadToEndAsync();
+        courseList = JsonSerializer.Deserialize<List<Course>>(contents);
+
+        return courseList;
     }
 }
